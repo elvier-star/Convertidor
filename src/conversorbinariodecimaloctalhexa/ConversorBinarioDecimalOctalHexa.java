a8c00d23d32cd9770f6d17942c45ff404c05ccc7
Elvier alejandro de la cruz quintero <107590729+elvier-star@users.noreply.github.com>
2022-07-11 20:08:38 -0500

    convertido

---
+++ src/conversorbinariodecimaloctalhexa/ConversorBinarioDecimalOctalHexa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package conversorbinariodecimaloctalhexa;
+
+import Gui.Principal;
+import Operacion.Convertir;
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Mipc
+ */
+public class ConversorBinarioDecimalOctalHexa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        System.out.println(""+new BigInteger("100").mod(new BigInteger("7")));
+//        System.out.println(""+(100%7));
+//        System.out.println(""+new BigInteger("0").compareTo(BigInteger.ZERO));// se compara si el num
+        new Principal().setVisible(true);
+//      
+       
+    }
+    
+}
